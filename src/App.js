7d43c6be9283e462a8b34f695b21ffb85a534024
sujin <asj9674@gmail.com>
2021-02-28 19:48:59 +0900

    component creation
    component creation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+            <h1>WEB</h1>
+            world wide web!
+      </header>
+    );
+  }
+}
+
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JAVASCRIPT</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+          <h2>HTML</h2>
+          HTML is Hyper Text Markup Language.
+      </article>
+    );
+  }
+}
+
+//컴포넌트를 만드는 코드 템플릿 
 class App extends Component {
   render() {
     return (
 
       <div className="App">
-        Hello, React!!
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>  
       </div>
       
     );
